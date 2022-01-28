eff809fdb50561b591d2b27556cbd814b099b7a6
Jakub Organa <jakub.organa@softwareplant.com>
2022-01-29 00:31:14 +0100

    Add basic routing

--- front/ui/src/app/app-routing.module.ts
+++ front/ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { LeaderboardsComponent } from './leaderboards/leaderboards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'leaderboards', component: LeaderboardsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/ui/src/app/app.module.ts
+++ front/ui/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main/main.component';
+import { LeaderboardsComponent } from './leaderboards/leaderboards.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LeaderboardsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
