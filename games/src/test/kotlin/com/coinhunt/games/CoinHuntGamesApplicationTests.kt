1066b0da5b58a84c0efdb0eee006c8efca0c0df0
Gerard Dróżdż <gerardd@interia.pl>
2022-02-10 21:01:47 +0100

    Initialize Spring Kotlin project

---
+++ games/src/test/kotlin/com/coinhunt/games/CoinHuntGamesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coinhunt.games
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoinHuntGamesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
