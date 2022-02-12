7598c2e9df1383059a83172ad6bcb3e4dbe5e65f
Jakub Organa <jakub.organa@softwareplant.com>
2022-02-12 21:18:43 +0100

    Add spring project

---
+++ front/service/src/main/java/com/coinhunt/front/CoinHuntFrontApplication.java
@@ -0,0 +1,13 @@
+package com.coinhunt.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinHuntFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinHuntFrontApplication.class, args);
+	}
+
+}
---
+++ front/service/src/test/java/com/coinhunt/front/CoinHuntFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinhunt.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinHuntFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
