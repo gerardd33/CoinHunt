06f40dfc89ed0a6eba9511eb9c476b97e52eb8a5
Jakub Organa <jakub.organa@softwareplant.com>
2022-02-14 08:30:54 +0100

    Add varnish

--- games/src/main/kotlin/com/coinhunt/games/api/controllers/GameController.kt
+++ games/src/main/kotlin/com/coinhunt/games/api/controllers/GameController.kt
@@ -8,8 +8,11 @@ import com.coinhunt.games.api.mappers.LevelInfoDtoMapper
 import com.coinhunt.games.api.mappers.MazeDtoMapper
 import com.coinhunt.games.common.GamesUtils.parseDifficulty
 import com.coinhunt.games.services.GameService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -21,8 +24,12 @@ class GameController(
     @Autowired private val mazeDtoMapper: MazeDtoMapper
 ) {
 
+    private val logger: Logger = LoggerFactory.getLogger("GameController")
+
     @GetMapping("/info/{difficulty}")
     fun getLevelInfo(@PathVariable difficulty: String): LevelInfoDto {
+        logger.info("---Handling /info")
+
         return levelInfoDtoMapper.domainToDto(gameService.retrieveGameMetadata(parseDifficulty(difficulty)))
     }
 
@@ -34,7 +41,13 @@ class GameController(
     }
 
     @GetMapping("/new/{difficulty}")
-    fun getNewGeneratedMaze(@PathVariable difficulty: String): MazeDto {
-        return mazeDtoMapper.domainToDto(gameService.generateNewMaze(parseDifficulty(difficulty)))
+    fun getNewGeneratedMaze(@PathVariable difficulty: String): ResponseEntity<MazeDto> {
+        val dto = mazeDtoMapper.domainToDto(gameService.generateNewMaze(parseDifficulty(difficulty)))
+
+        logger.info("---Handling /new")
+
+        return ResponseEntity.ok()
+                .header("Cache-Control", "no-cache")
+                .body(dto)
     }
 }
